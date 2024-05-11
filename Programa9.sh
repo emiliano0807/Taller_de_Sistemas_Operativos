042c42866a78efe5747d9d822ba6ebf1313ea2ef
emiliano0807 <emilianorivfacio@gmail.com>
2024-05-10 18:35:49 -0600

    Ultimo codigo en sh del curso

---
+++ Programa9.sh
@@ -0,0 +1,40 @@
+while true
+do
+echo "Menu Principal"
+echo "1.- Tabla de miltiplicar"
+echo "2.- Factorial de X numero"
+echo "3.- Tablas de multiplicar"
+echo "4.- Z²=X²+Y²"
+echo "5.- Edad"
+echo "6.- N° de control"
+echo "7.- Salir"
+echo "Elige una opcion"
+read x
+case $x in
+1)
+./Programa2.sh
+;;
+2)
+./Programa3.sh
+;;
+3)
+./Programa4.sh
+;;
+4)
+./Programa6.sh
+;;
+5)
+./Programa7.sh
+;;
+6)
+./Programa8.sh
+;;
+7)
+clear
+exit
+esac
+done
+
+
+ 
+
