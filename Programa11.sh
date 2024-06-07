3189589bf89f653fd00f50c7dcb891cb651be2f7
emiliano0807 <emilianorivfacio@gmail.com>
2024-06-07 08:53:04 -0600

    Programa12 commit

---
+++ Programa11.sh
@@ -0,0 +1,12 @@
+echo "Dame el segmento de red a trabajar"
+read x
+echo "Dime el numero de host inicial"
+read y
+echo "Dime el numero del host final"
+read z
+	while [ $y -le $z ]
+do
+iptables -D INPUT -p tcp -s $x$y --dport 22 -j DROP
+y=`expr $y \+ 1`
+done
+/sbin/iptables -nL
